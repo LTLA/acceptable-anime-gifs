9b3906a038839d64708bb787e6e442a254c68128
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-06-04 00:29:47 -0700

    Initial commit.

---
+++ slackbot.R
@@ -0,0 +1,22 @@
+choices <- read.csv("acceptable.tsv", header=FALSE)
+current <- sample(acceptable[,1], 1)
+
+hook <- commandArgs(trailing=TRUE)[1]
+
+library(httr)
+out <- POST(hook,
+     body=sprintf('{
+    "text":"A periodic anime GIF.",
+    "blocks":[
+        {
+            "type": "image",
+            "title": {
+                "type": "plain_text",
+                "text": "Some anime to cheer you up; you deserve it."
+            },
+            "image_url": "%s",
+            "alt_text": "Gochisousama!"
+        }
+    ]
+}', current), encode='raw', content_type("json"))
+
